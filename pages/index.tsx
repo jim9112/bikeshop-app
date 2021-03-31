53c327443a0d9a844863b354a1d1caf5ab4cef92
jim9112 <jim9112@gmail.com>
2021-03-31 11:43:54 -0400

    add typescript and tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <header>
+          <h1 className="text-center">Bike Shop App</h1>
+        </header>
+      </main>
+    </div>
+  );
+}
