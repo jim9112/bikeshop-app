777ffd769e83c6218ed03ef7bdc087b500da9e3a
jim9112 <jim9112@gmail.com>
2021-04-05 18:08:20 -0400

    create main page

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="bg-white text-center">
+      <h1>Bike Shop Work Order System</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ pages/main.tsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header';
+
+const Main = () => {
+  return (
+    <div className="h-full">
+      <Header />
+      <h1>Main Page</h1>
+    </div>
+  );
+};
+
+export default Main;
